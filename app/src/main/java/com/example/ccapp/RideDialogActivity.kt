caa03d96beea3d3c097d6ae1e1d361b61eeaf1e8
Jooolioh <giulio2797@gmail.com>
2022-05-12 21:51:20 +0200

    Base activity files

---
+++ app/src/main/java/com/example/ccapp/RideDialogActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ccapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RideDialogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ride_dialog)
+    }
+}
---
+++ app/src/main/java/com/example/ccapp/RideRecordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ccapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RideRecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ride_record)
+    }
+}
