fbcb5dacb6132d13e2ee2d78737902fb1203065a
Jooolioh <giulio2797@gmail.com>
2022-05-13 14:32:37 +0200

    Profile with image not working

--- app/src/main/java/com/example/ccapp/ProfileActivity.kt
+++ app/src/main/java/com/example/ccapp/ProfileActivity.kt
@@ -2,10 +2,20 @@ package com.example.ccapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var layout: ViewGroup
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        layout = findViewById(R.id.button_layout)
+        val editButton = Button(this)
+        editButton.setText("Edit")
+        layout.addView(editButton)
     }
 }
