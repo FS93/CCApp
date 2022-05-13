d3393d52030032a5a84a61db2af2e8c89fcb66d7
Jooolioh <giulio2797@gmail.com>
2022-05-13 13:48:13 +0200

    Home activity added menu and basic links to other activites

--- app/src/main/java/com/example/ccapp/HomeActivity.kt
+++ app/src/main/java/com/example/ccapp/HomeActivity.kt
@@ -1,11 +1,50 @@
 package com.example.ccapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    //data = getIntent().getExtras().getString("keyName","defaultKey");
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.search -> {
+                val intent = Intent(this@HomeActivity, RideDialogActivity::class.java)
+                intent.putExtra("dialog_type", "search")
+                finish()
+                startActivity(intent)
+            }
+            R.id.offer -> {
+                val intent = Intent(this@HomeActivity, RideDialogActivity::class.java)
+                intent.putExtra("dialog_type", "offer")
+                finish()
+                startActivity(intent)
+            }
+            R.id.profile -> {
+                val intent = Intent(this@HomeActivity, ProfileActivity::class.java)
+                finish()
+                startActivity(intent)
+            }
+            R.id.logout -> {
+                val intent = Intent(this@HomeActivity, RideDialogActivity::class.java)
+                finish()
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
