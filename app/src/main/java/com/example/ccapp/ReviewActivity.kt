ca1f13a85ec2aaf65969330af9f873b9c2bf0bd2
flck <florian.schroeck@stud.htwk-leipzig.de>
2022-05-14 23:45:35 +0200

    Designed Review Activity with RecylcerView

--- app/src/main/java/com/example/ccapp/ReviewActivity.kt
+++ app/src/main/java/com/example/ccapp/ReviewActivity.kt
@@ -2,10 +2,35 @@ package com.example.ccapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_review.*
 
 class ReviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review)
+
+        rvReviews.layoutManager = LinearLayoutManager(this)
+
+        var reviewsList = mutableListOf(
+            Review("Paolo Rossi", R.drawable.avatar),
+            Review("Max Mustermann", R.drawable.avatar),
+            Review("Mario Draghi", R.drawable.avatar),
+            Review("Olaf Scholz", R.drawable.avatar),
+            Review("Paolo Rossi", R.drawable.avatar),
+            Review("Max Mustermann", R.drawable.avatar),
+            Review("Mario Draghi", R.drawable.avatar),
+            Review("Olaf Scholz", R.drawable.avatar),
+            Review("Paolo Rossi", R.drawable.avatar),
+            Review("Max Mustermann", R.drawable.avatar),
+            Review("Mario Draghi", R.drawable.avatar),
+            Review("Olaf Scholz", R.drawable.avatar)
+        )
+
+        val adapter = ReviewAdapter(reviewsList)
+
+        rvReviews.adapter = adapter
+
     }
 }
