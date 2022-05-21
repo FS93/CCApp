93c29dc4a6e6c07b063aeb48f7abf24e04e2fbfe
flck <florian.schroeck@stud.htwk-leipzig.de>
2022-05-21 13:01:42 +0200

    Redesign of the RideRecordActivity

--- app/src/main/java/com/example/ccapp/RideRecordActivity.kt
+++ app/src/main/java/com/example/ccapp/RideRecordActivity.kt
@@ -3,30 +3,51 @@ package com.example.ccapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_ride_record.*
+import java.util.regex.Pattern
 
 class RideRecordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ride_record)
 
-        val layoutManager = LinearLayoutManager(this)
-        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
-        rvPassengers.layoutManager = layoutManager
+        val departure = findViewById<TextView>(R.id.txtRideRecordDeparture)
+        val destination = findViewById<TextView>(R.id.txtRideRecordDestination)
 
-        var PassengerList = mutableListOf<Passenger>(
-            Passenger("Francesca Maria"),
-            Passenger("Giulio"),
-            Passenger(false),
-            Passenger(false)
-        )
+        // Insert line breaks for long texts
+            departure.text.replace(Regex(","), "\n")
+            destination.text.replace(Regex(","), "\n")
 
-        val adapter = PassengerAdapter(PassengerList)
+        // Setup RecyclerView for passengers
 
-        rvPassengers.adapter = adapter
+            // Create LinearLayoutManager without horizontal scrolling
+            val layoutManager = object : LinearLayoutManager(this) {
+                override fun canScrollHorizontally(): Boolean {
+                    return false
+                }
+            }
+
+            layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+            rvPassengers.layoutManager = layoutManager
+
+            var PassengerList = mutableListOf<Passenger>(
+                Passenger("Francesca Maria"),
+                Passenger("Giulio"),
+                Passenger(false),
+                Passenger(false)
+            )
+
+
+            val adapter = PassengerAdapter(PassengerList)
+            rvPassengers.adapter = adapter
+
+
 
 
     }
+
+
 
 }
